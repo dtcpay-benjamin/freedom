7db71257331f900597c1631daee87b4d1e51946d
褚红彪 <hujiao_chuhongbiao@163.com>
2025-06-24 10:48:04 +0800

    update:立即开通会员按钮封装完善。

--- seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTMemberImmediatelyCell.h
+++ seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTMemberImmediatelyCell.h
@@ -11,6 +11,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SHTMemberImmediatelyCell : UITableViewCell
 
+@property (nonatomic, strong) void (^memberImmediatelyOnTapped)(void); // 开通会员点击回调
+
+@property (nonatomic, strong) void (^radioOnTapped)(BOOL isSelected); // 已阅读会员服务协议点击回调
+
+@property (nonatomic, strong) void (^serviceAgreementOnTapped)(void); // 会员服务协议点击回调
+
 @end
 
 NS_ASSUME_NONNULL_END
--- seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTMemberImmediatelyCell.m
+++ seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTMemberImmediatelyCell.m
@@ -6,6 +6,7 @@
 //
 
 #import "SHTMemberImmediatelyCell.h"
+#import <Masonry/Masonry.h>
 
 @interface SHTMemberImmediatelyCell()
 
@@ -17,22 +18,76 @@
 
 @implementation SHTMemberImmediatelyCell
 
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        [self addSubviews];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self addLayoutSubviews];
+}
+
+- (void)addSubviews {
+    [self.contentView addSubview:self.immediatelyBtn];
+    [self.contentView addSubview:self.radioButton];
+    [self.contentView addSubview:self.agreementTextLabel];
+}
+
+
+- (void)addLayoutSubviews {
+    [self.immediatelyBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self).offset(10);
+        make.leading.equalTo(self).offset(30);
+        make.trailing.equalTo(self).offset(-30);
+        make.height.mas_equalTo(80);
+    }];
+    
+    [self.radioButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.immediatelyBtn.mas_bottom).offset(10);
+        make.leading.equalTo(self.immediatelyBtn.mas_leading).offset(10);
+        make.width.mas_equalTo(22);
+        make.height.mas_equalTo(22);
+    }];
+    
+    
+    [self.agreementTextLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.radioButton);
+        make.leading.equalTo(self.radioButton.mas_trailing).offset(10);
+        make.trailing.equalTo(self.immediatelyBtn.mas_trailing).offset(-10);
+        make.height.mas_equalTo(22);
+    }];
+}
 
 #pragma mark - actions
 
 // 立即开通
 - (void)immediatelyAction:(UIButton *)sender {
-
+    if (self.memberImmediatelyOnTapped) {
+        self.memberImmediatelyOnTapped();
+    }
 }
 
 // 选择阅读协议
 - (void)radioAction:(UIButton *)sender {
-    
+    sender.selected = !sender.isSelected;
+    if (self.radioOnTapped) {
+        self.radioOnTapped(sender.isSelected);
+    }
+}
+
+// 会员服务协议点击跳转
+- (void)serviceAgreementHandleTapOn:(UITapGestureRecognizer *)tap {
+    if (self.serviceAgreementOnTapped) {
+        self.serviceAgreementOnTapped();
+    }
 }
 
 #pragma mark - 懒加载
 
-- (UIButton *)immediatelyBtn{
+- (UIButton *)immediatelyBtn {
     if (!_immediatelyBtn) {
         _immediatelyBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         _immediatelyBtn.titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -57,9 +112,25 @@
     return _radioButton;
 }
 
-- (UILabel *)agreementTextLabel{
+- (UILabel *)agreementTextLabel {
     if (!_agreementTextLabel) {
         _agreementTextLabel = [[UILabel alloc] init];
+        _agreementTextLabel.userInteractionEnabled = YES;
+        // 文字内容
+        NSString *fullText = @"开通前请阅读《会员服务协议》（含自动续费条款）";
+        NSString *linkText = @"《会员服务协议》";
+        // 构造富文本
+        NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:fullText];
+        // 设置整体字体和颜色
+        [attrStr addAttribute:NSFontAttributeName value:SHTUIFontSystem(14) range:NSMakeRange(0, fullText.length)];
+        [attrStr addAttribute:NSForegroundColorAttributeName value:SHTUIColorFromRGB(153.0, 147.0, 137.0) range:NSMakeRange(0, fullText.length)];
+        // 设置链接部分样式
+        NSRange linkRange = [fullText rangeOfString:linkText];
+        [attrStr addAttribute:NSForegroundColorAttributeName value:SHTUIColorFromRGB(163.0, 157.0, 148.0) range:linkRange];
+        [attrStr addAttribute:NSUnderlineStyleAttributeName value:@(NSUnderlineStyleSingle) range:linkRange];
+        _agreementTextLabel.attributedText = attrStr;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(serviceAgreementHandleTapOn:)];
+        [_agreementTextLabel addGestureRecognizer:tap];
     }
     return _agreementTextLabel;
 }
