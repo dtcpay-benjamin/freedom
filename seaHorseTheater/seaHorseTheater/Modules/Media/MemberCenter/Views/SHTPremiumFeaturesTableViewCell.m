cb6116a1b5bcf954f1639389f0eee69c92ff93e1
Benjamin <ext.benjamin.chu@dtcpay.com>
2025-07-06 23:06:15 +0800

    update:控件封装。

--- seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTPremiumFeaturesTableViewCell.m
+++ seaHorseTheater/seaHorseTheater/Modules/Media/MemberCenter/Views/SHTPremiumFeaturesTableViewCell.m
@@ -6,6 +6,7 @@
 //
 
 #import "SHTPremiumFeaturesTableViewCell.h"
+#import <Masonry/Masonry.h>
 
 @interface SHTPremiumFeaturesView()
 
@@ -17,9 +18,68 @@
 
 @implementation SHTPremiumFeaturesView
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self addSubviews];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addSubviews];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [self addLayoutSubviews];
+}
+
+- (void)addSubviews {
+    [self addSubview:self.featuresImageView];
+    [self addSubview:self.titleLabel];
+    [self addSubview:self.subTitleLabel];
+}
+
+- (void)addLayoutSubviews {
+    [self.featuresImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self);
+        make.centerX.equalTo(self);
+        make.width.mas_equalTo(60);
+        make.height.mas_equalTo(60);
+    }];
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.featuresImageView.mas_bottom).offset(10);
+        make.centerX.equalTo(self);
+        make.height.mas_equalTo(18);
+    }];
+    [self.subTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.titleLabel.mas_bottom).offset(5);
+        make.centerX.equalTo(self);
+        make.height.mas_equalTo(18);
+    }];
+}
+
 - (void)setData:(NSDictionary *)data {
     _data = data;
-    
+    NSString *imgStr = _data[@"featuresImage"];
+    if (imgStr && imgStr.length > 0) {
+        self.featuresImageView.image = [UIImage imageNamed:imgStr];
+    }
+    NSString *titleStr = _data[@"title"];
+    if (titleStr && titleStr.length > 0) {
+        self.titleLabel.text = titleStr;
+    }
+    NSString *subTitleStr = _data[@"subTitle"];
+    if (subTitleStr && subTitleStr.length > 0) {
+        self.subTitleLabel.text = subTitleStr;
+    }
 }
 
 #pragma mark - 懒加载
@@ -27,8 +87,31 @@
 - (UIImageView *)featuresImageView {
     if (!_featuresImageView) {
         _featuresImageView = [[UIImageView alloc] init];
+        _featuresImageView.layer.masksToBounds = YES;
+        _featuresImageView.layer.cornerRadius = 8.0;
+        _featuresImageView.backgroundColor = SHTUIColorFromRGB(39, 32, 29);
     }
     return _featuresImageView;
+}
+
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] init];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        _titleLabel.textColor = SHTUIColorFromRGB(138, 131, 123);
+        _titleLabel.font = SHTUIFontSystem(16);
+    }
+    return _titleLabel;
+}
+
+- (UILabel *)subTitleLabel {
+    if (!_subTitleLabel) {
+        _subTitleLabel = [[UILabel alloc] init];
+        _subTitleLabel.textAlignment = NSTextAlignmentCenter;
+        _subTitleLabel.textColor = SHTUIColorFromRGB(80, 77, 70);
+        _subTitleLabel.font = SHTUIFontSystem(14);
+    }
+    return _subTitleLabel;
 }
 
 @end
